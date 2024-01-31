0d8c4e09e6b498e68d7138f8f0953bf369b9c66f
LuizEduardoSC <luizeduardoedd1@gmail.com>
2024-01-31 11:29:42 -0300

    rotas-conclusao-do-projeto

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
     <div className="grid grid-cols-2 bg-gray-800 text-gray-200">
         <div className="flex flex-col gap-6 items-center justify-center">
             <h1 className="text-5xl">Seja bem vindo!</h1>
-            <p className="text-lg">Aqui você encontra os melhores Produtos!</p>
+            <p className="text-lg">Aqui você encontra os melhores Medicamentos!</p>
             <button className="rounded border-solid border-2 w-1/5 py-2 hover:border-purple-600">Novo Produto</button>
         </div>
         <div id="imagem" className='flex justify-center'>
